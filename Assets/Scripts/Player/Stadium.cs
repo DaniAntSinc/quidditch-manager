061fa84fa5a70af45888b8109d948ae508989b7a
DaniAntSinc <cub.noble@yahoo.fr>
2025-01-23 18:01:22 -0600

    Management: Starting in on Stadium elements

--- Assets/Scripts/Player/Stadium.cs
+++ Assets/Scripts/Player/Stadium.cs
@@ -7,4 +7,8 @@ public class Stadium : MonoBehaviour
     public string stadiumName;
     public float hotChance, clearChance, fogChance, rainChange, snowChance, indoorChance;
     public int capacity;
+    public float consessionPrice;
+    public float consessionRangeMin, concessionRangeMax;
+    public float souvenirPrice;
+    public float souvenirRangeMin, souvenirRangeMax;
 }
---
+++ Assets/StadiumElement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StadiumElement : MonoBehaviour
+{
+    public int cost;
+    public bool capacity;
+    public int capacityToAdd;
+    public bool concessions;
+    public float concessionsToAdd;
+    public float percentConcessionsToAdd;
+    public bool souvenir;
+    public float souvenirToAdd;
+    public float percentSouvenirToAdd;
+
+    public string saveElementTrigger;
+    //put a string in here that matches the save_load component. if the bool = 1 on load, destroy this. 
+
+    public void UpdatePlayersStadiumStats()
+    {
+        if (GameObject.Find("Management").GetComponent<Management>().teamBudget >= cost)
+        {
+            GameObject.Find("Management").GetComponent<Management>().teamBudget -= cost;
+
+            if (capacity)
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.capacity += capacityToAdd;
+            else if (concessions)
+            {
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.consessionPrice += concessionsToAdd;
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.concessionRangeMax += percentConcessionsToAdd;
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.consessionRangeMin += percentConcessionsToAdd;
+            }
+            else if (souvenir)
+            {
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.souvenirPrice += souvenirToAdd;
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.souvenirRangeMin += percentSouvenirToAdd;
+                GameObject.Find("Players_Team").GetComponent<SeasonTeam>().homeStadium.souvenirRangeMax += percentSouvenirToAdd;
+            }
+
+            //update text at bottom of the screen
+
+            Destroy(this.gameObject);
+        }
+    }
+}
