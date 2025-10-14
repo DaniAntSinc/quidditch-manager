bc9f6743f2a8eed1fa229c933c772eaa9fa6f26a
DaniAntSinc <cub.noble@yahoo.fr>
2025-10-13 23:31:55 -0500

    Playoffs: Further laying out the Playoff menu

--- Assets/PlayoffMenu.cs
+++ Assets/PlayoffMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayoffMenu : MonoBehaviour
 {
     public GameObject GameModeMenuGO, PlayoffMenuGO;
+    public GameObject highlighter;
+    public GameObject[] firstRound, secondRound, thirdRound, winner;
+
+    public List <SeasonTeam> visitorTeam, homeTeam;
     public void ClosePlayoffs()
     {
         GameModeMenuGO.SetActive(true);
